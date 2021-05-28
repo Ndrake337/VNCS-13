a833fb3f1183ae5b761aa19f499751a2da7b4faa
Ndrake337 <matheuws.felipe337@gmail.com>
2021-05-28 20:06:30 -0300

    Init

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+state = { "value" : 1} 
+
+@app.route('/', methods =['GET'])
+def home():
+    return "Seja Bem Vindo ao VNCS-13",200
+
+@app.route('/GetState', methods =['GET'])
+def home():
+    return jsonify(state),200
+
+@app.route('/AppChangeState', methods =['GET'])
+def AppChangeState():
+    if state['value'] == 1:
+        state['value'] = 0
+    else:
+        state['value'] = 1
+
+    return jsonify(state),200
+
+@app.route('/NodeChangeState', methods =['POST'])
+def NodeChangeState():
+    data = request.get_json()
+    state.update(data)
+
+    return jsonify(data),201
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
