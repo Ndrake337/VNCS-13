67b0436874c6bdbae5655be95edbe56fff759f46
Matheus Felipe Neves Campos Cardoso <matheus.felipe337@gmail.com>
2021-05-28 20:55:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask, jsonify, request
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 state = { "value" : 1} 
 
-@app.route('/', methods =['GET'])
+@application.route('/', methods =['GET'])
 def home():
     return "Seja Bem Vindo ao VNCS-13",200
 
-@app.route('/GetState', methods =['GET'])
+@application.route('/GetState', methods =['GET'])
 def home():
     return jsonify(state),200
 
-@app.route('/AppChangeState', methods =['GET'])
+@application.route('/AppChangeState', methods =['GET'])
 def AppChangeState():
     if state['value'] == 1:
         state['value'] = 0
@@ -21,7 +21,7 @@ def AppChangeState():
 
     return jsonify(state),200
 
-@app.route('/NodeChangeState', methods =['POST'])
+@application.route('/NodeChangeState', methods =['POST'])
 def NodeChangeState():
     data = request.get_json()
     state.update(data)
